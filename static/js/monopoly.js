cef68d6132327e0b5c486df328e8ed2ea6b2696c
Seamus Allsion <smallison3409@gmail.com>
2025-09-17 20:07:32 -0400

    Updated front end for an accurate monopoly board.

--- static/js/monopoly.js
+++ static/js/monopoly.js
@@ -1,21 +1,11 @@
-import * as mono from "./monopoly_functions.js";
+
+
 const socket = io({ autoConnect: true });
 
 
 
 socket.on("join", (data) => {
     console.log(data.message);
-    let title = data.title;
-    document.getElementById("player").innerHTML=title;
-    let monopoly_1 = document.getElementById("monopoly-1");
-
-
-    var tile = 0;
-    let overLay = 1;
-    let overLayElements = document.querySelectorAll(".corner, .tile, .rtile, .ltile");
-
-    mono.moveTo(monopoly_1, 0);
-    mono.input(document, monopoly_1, overLayElements, tile, overLay, socket);
 });
 
 socket.on("rolled", (data)=>{
@@ -28,73 +18,92 @@ socket.on("roll_broadcast", (data) => {
 
 
 
-// Find game board parts
-const gameBoard = document.querySelector("#gameboard");
-const playerDisplay = document.querySelector("#player");
-const infoDiplay = document.querySelector("#info-display");
+socket.on("join", (data) => {
+    console.log(data.message);
+});
 
 
-//  Center, right side, and left side pieces
-const pieces = [
-    "c",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "c",
-    "lt",
-    "center",
-    "rt",
-    "lt",
-    "center",
-    "rt",
-    "lt",
-    "center",
-    "rt",
-    "lt",
-    "center",
-    "rt",
-    "lt",
-    "center",
-    "rt",
-    "lt",
-    "center",
-    "rt",
-    "lt",
-    "center",
-    "rt",
-    "lt",
-    "center",
-    "rt",
-    "lt",
-    "center",
-    "rt",
-    "c",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "t",
-    "c",
-];
 
-const ids = [
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 20, 32, 19, 33, 18, 34, 17, 35,
-    16, 36, 15, 37, 14, 38, 13, 39, 12, 40, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
-];
-// tile IDs
 
-// Create HTML elements from pieces with ids
-mono.createBoard(pieces, gameBoard, ids);
+
+var Tiles = { "List" : [
+  {"id":"21" ,"label":"strand", "price":"£220", "icon":"", "color":"red", "order": "2", "pos":"top"},
+  {"id":"22" ,"label":"chance", "price":"", "icon":"❓", "color":"none", "order": "3", "pos":"top"},
+  {"id":"23" ,"label":"fleet street", "price":"£220", "icon":"", "color":"red", "order": "4", "pos":"top"},
+  {"id":"24" ,"label":"trafalgar square", "price":"£240", "icon":"", "color":"none", "order": "5", "pos":"top"},
+  {"id":"25" ,"label":"fenchurch st. station", "price":"£200", "icon":"🚂", "color":"none", "order": "6", "pos":"top"},
+  {"id":"26" ,"label":"leicester square", "price":"£260", "icon":"", "color":"yellow", "order": "7", "pos":"top"},
+  {"id":"27" ,"label":"ceventry street", "price":"£200", "icon":"", "color":"yellow", "order": "8", "pos":"top"},
+  {"id":"28" ,"label":"water<br>works", "price":"£150", "icon":"🚰", "color":"none", "order": "9", "pos":"top"},
+  {"id":"29" ,"label":"piccadilly", "price":"£280", "icon":"", "color":"yellow", "order": "10", "pos":"top"},
+  {"id":"19" ,"label":"vine street", "price":"£200", "icon":"", "color":"orange", "order": "12", "pos":"left"},
+  {"id":"18" ,"label":"marlborog'h street", "price":"£200", "icon":"", "color":"orange", "order": "15", "pos":"left"},
+  {"id":"17" ,"label":"community chest", "price":"", "icon":"💰", "color":"none", "order": "17", "pos":"left"},
+  {"id":"16" ,"label":"bow street", "price":"£180", "icon":"", "color":"orange", "order": "19", "pos":"left"},
+  {"id":"15" ,"label":"marylebone station", "price":"£200", "icon":"🚂", "color":"none", "order": "21", "pos":"left"},
+  {"id":"14" ,"label":"northumrl'd avenue", "price":"£160", "icon":"", "color":"pink", "order": "23", "pos":"left"},
+  {"id":"13" ,"label":"whitehall", "price":"£140", "icon":"", "color":"pink", "order": "25", "pos":"left"},
+  {"id":"12" ,"label":"electric company", "price":"£150", "icon":"💡", "color":"none", "order":" 27", "pos":"left"},
+  {"id":"11" ,"label":"pall mall", "price":"£140", "icon":"", "color":"pink", "order": "29", "pos":"left"},
+  {"id":"31" ,"label":"regent street", "price":"£300", "icon":"", "color":"green", "order": "14", "pos":"right"},
+  {"id":"32" ,"label":"oxford street", "price":"£300", "icon":"", "color":"green", "order": "16", "pos":"right"},
+  {"id":"33" ,"label":"community chest", "price":"", "icon":"💰", "color":"none", "order": "18", "pos":"right"},
+  {"id":"34" ,"label":"bond street", "price":"£320", "icon":"", "color":"green", "order": "20", "pos":"right"},
+  {"id":"35" ,"label":"liverpool st. station", "price":"£320", "icon":"🚂", "color":"none", "order": "22", "pos":"right"},
+  {"id":"36" ,"label":"chance", "price":"", "icon":"", "color":"none", "order": "24", "pos":"right"},
+  {"id":"37" ,"label":"park lane", "price":"£350", "icon":"", "color":"blue", "order": "26", "pos":"right"},
+  {"id":"38" ,"label":"super<br />tax", "price":"£100", "icon":"💍", "color":"none", "order": "28", "pos":"right"},
+  {"id":"39" ,"label":"mayfair", "price":"£400", "icon":"", "color":"blue", "order": "30", "pos":"right"},
+  {"id":"9" ,"label":"pentonville road", "price":"£120", "icon":"", "color":"sky", "order": "32", "pos":"bottom"},
+  {"id":"8" ,"label":"euston road", "price":"£100", "icon":"", "color":"sky", "order": "33", "pos":"bottom"},
+  {"id":"7" ,"label":"chance", "price":"", "icon":"❓", "color":"none", "order": "34", "pos":"bottom"},
+  {"id":"6" ,"label":"the angel, islington", "price":"£100", "icon":"", "color":"sky", "order": "35", "pos":"bottom"},
+  {"id":"5" ,"label":"kings cross station", "price":"£200", "icon":"🚂", "color":"none", "order": "36", "pos":"bottom"},
+  {"id":"4" ,"label":"income<br />tax", "price":"£200", "icon":"🔸", "color":"none", "order": "37", "pos":"bottom"},
+  {"id":"3" ,"label":"whitechapel road", "price":"£60", "icon":"", "color":"brown", "order": "38", "pos":"bottom"},
+  {"id":"2" ,"label":"community chest", "price":"", "icon":"💰", "color":"none", "order": "39", "pos":"bottom"},
+  {"id":"1" ,"label":"old kent road", "price":"£60", "icon":"", "color":"brown", "order": "40", "pos":"bottom"}
+]};
+
+var tiles = "";
+
+for (var t = 0; t < Tiles.List.length; t++){
+tiles += "<div id=\"" + Tiles.List[t].id + "\" class=\"" + Tiles.List[t].pos + " " + Tiles.List[t].color + "\" style=\"--order:" + Tiles.List[t].order + ";\"><div class=\"inside\"><h2>" + Tiles.List[t].label + "</h2> <span>" + Tiles.List[t].icon + "</span> <strong>" + Tiles.List[t].price + "</strong></div></div>"
+}
+
+$(".frame").append(tiles);
+
+
+
+const player = document.createElement('img');
+player.src = "/static/img/monopoly_piece_1.svg";
+player.id="monopoly-1";
+player.className="piece";
+
+document.getElementById("2").appendChild(player);
+
+
+
+socket.on("rolled", (data) => {
+
+
+    console.log(data["current_position"]);
+    document.getElementById(data["current_position"]).appendChild(player);
+});
+
+
+
+document.addEventListener("keydown", function (event) {
+        // Check the key code of the pressed key
+        switch (event.code) {
+            case "KeyR":
+                socket.emit("roll");
+
+                break;
+        }
+
+
+    });
 
 
 
