0697969b401c2bdb6c3a9cca5fa88c7ac5d4c9a6
JackChuck1 <134446836+JackChuck1@users.noreply.github.com>
2025-09-24 14:03:46 +0000

    Added position value

--- routes/monopoly/monopoly_routes.py
+++ routes/monopoly/monopoly_routes.py
@@ -45,4 +45,32 @@ def register_routes(app, db: SQLAlchemy):
     @login_required
     def monopoly():
         return render_template("monopoly.html")
-
+    
+    #going to ahsofware.club/reset_properties will delete all property data and reload it
+    @app.route("/reset_properties")
+    @login_required
+    def reset_properties():
+        with app.app_context():
+            db.create_all()
+        data = pd.read_csv("properties.csv", index_col=0)
+        for property in Property.query:
+            db.session.delete(property)
+        db.session.commit
+        for title in data.index.values:
+            property = Property(title=title,
+                                    price=int(data.loc[title].iloc[0]),
+                                    rent_no_set=int(data.loc[title].iloc[1]),
+                                    rent_color_set=int(data.loc[title].iloc[2]),
+                                    rent_1_house=int(data.loc[title].iloc[3]),
+                                    rent_2_house=int(data.loc[title].iloc[4]),
+                                    rent_3_house=int(data.loc[title].iloc[5]),
+                                    rent_4_house=int(data.loc[title].iloc[6]),
+                                    rent_hotel=int(data.loc[title].iloc[7]),
+                                    building_cost=int(data.loc[title].iloc[8]),
+                                    mortgage=int(data.loc[title].iloc[9]),
+                                    unmortgage=int(data.loc[title].iloc[10]),
+                                    position=int(data.loc[title].iloc[11]),
+                                    color=data.loc[title].iloc[-1])
+            db.session.add(property)
+        db.session.commit()
+        return render_template("monopoly.html")
